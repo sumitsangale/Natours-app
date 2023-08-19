c01802c6d45a170632e956e9a84e478480331f14
sumit sangale <sangalesumit12@gmail.com>
2023-08-19 23:43:29 +0530

    create middleware to restrict specific roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -85,3 +85,19 @@ exports.protectRoute = catchAsync(async (req, res, next) => {
   req.user = freshUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    //here roles = ['admin', 'lead-guide']
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action.', 403)
+      );
+    }
+    next();
+  };
+};
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {});
