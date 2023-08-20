15fda6bee138aea526ec81842e8522e013ed4c21
sumit sangale <sangalesumit12@gmail.com>
2023-08-21 00:09:07 +0530

    added helmet packege for security headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,10 +11,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GOBAL MIDDLEWARES
+// Set security headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//limit request from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -21,9 +27,13 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
+//body parser to read from request body
 app.use(express.json());
+
+//serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
