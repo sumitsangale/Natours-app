57f706d493adbd694fda7bb0750d8d84a8b57cb6
sumit sangale <sangalesumit12@gmail.com>
2023-08-27 01:18:41 +0530

    header , footer created using pug

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const path = require('path');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,7 +12,15 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+//setting pug template
+app.set('veiw engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) GOBAL MIDDLEWARES
+//serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set security headers
 app.use(helmet());
 
@@ -31,9 +40,6 @@ app.use('/api', limiter);
 //body parser to read from request body
 app.use(express.json());
 
-//serving static files
-app.use(express.static(`${__dirname}/public`));
-
 //test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -41,6 +47,13 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base.pug', {
+    tour: 'The Forest Hiker',
+    user: 'sumit'
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
