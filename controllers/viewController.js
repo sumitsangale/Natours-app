35534c3b4290891c55b1dace5db3be2b1a270cef
sumit sangale <sangalesumit12@gmail.com>
2023-08-27 17:07:47 +0530

    deatail tour screen created

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -5,15 +5,23 @@ exports.getOverview = catchAsync(async (req, res) => {
   //get all tours
   const tours = await Tour.find();
 
-  //create templete
+  //create templete usring response
   res.status(200).render('overview.pug', {
     title: 'All tours',
     tours
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour.pug', {
-    title: 'The Forest Hiker'
+exports.getTour = catchAsync(async (req, res) => {
+  //get req tour data
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  //create teplate using response data
+  res.status(200).render('tour.pug', {
+    title: 'The Forest Hiker',
+    tour
+  });
+});
