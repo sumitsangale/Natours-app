4694931645380713af2fa66654409223613d8d52
sumit sangale <sangalesumit12@gmail.com>
2023-09-03 01:39:29 +0530

    implement bookings Api

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,11 +3,18 @@ const authController = require('../controllers/authController');
 const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
+router.use(authController.protectRoute);
+router.get('/checkout-session/:tourID', bookingController.getCheckoutSession);
 
-router.get(
-  '/checkout-session/:tourID',
-  authController.protectRoute,
-  bookingController.getCheckoutSession
-);
+router.use(authController.restrictTo('admin', 'lead-guide'));
+router
+  .route('/')
+  .get(bookingController.getAllBooking)
+  .post(bookingController.createBooking);
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
