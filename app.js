c48adf9b48d616b61fade4c76a48cd77d82a3f1e
sumit sangale <sangalesumit12@gmail.com>
2023-09-03 16:13:59 +0530

    prepare files for deployment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -81,6 +82,9 @@ app.use(
     ]
   })
 );
+
+//send response text with compress version
+app.use(compression());
 
 //test middleware
 app.use((req, res, next) => {
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -4,9 +4,7 @@ import { showAlert } from './alert';
 export const bookTour = async (tourID, stripe) => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://localhost:3000/api/v1/bookings/checkout-session/${tourID}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourID}`);
 
     // 2) create checkout form + charge credit card
     await stripe.redirectToCheckout({
